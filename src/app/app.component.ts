29cbb243e3e503d235a4a16eee448eca4fdce584
Miguel <m.ramosmolero@gmail.com>
2018-06-10 16:40:51 +0200

    Subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,59 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  title = 'el mejor Gráfico!';
+
+  private chartData: Array<any>;
+
+
+
+  //constructor(private _ApiService: ApiService) { }
+
+
+
+  ngOnInit() {
+
+
+
+    // Local Data //////////////////////////////////////////////////////////////////////
+
+
+
+    this.generateData();
+
+    // change the data periodically
+
+    //setInterval(() => this.generateData(), 3000);
+
+
+
+    // API Data ////////////////////////////////////////////////////////////////////////
+
+    //this._ApiService.getData().subscribe(result => { this.chartData = result })
+
+  }
+
+
+
+  generateData() {
+
+    this.chartData = [];
+
+    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
+
+      this.chartData.push([
+
+        `Index ${i}`,
+
+        Math.floor(Math.random() * 100)
+
+      ]);
+
+    }
+
+  }
+
 }
